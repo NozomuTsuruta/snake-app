2b923bb0bac4126529b8244a330ea816b01a22fc
NozomuTsuruta <tuno.0806@icloud.com>
2020-12-09 00:59:24 +0900

    点が動くように

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,53 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Navigation } from "./components/Navigation";
 import { Field } from "./components/Field";
 import { Button } from "./components/Button";
 import { ManipulationPanel } from "./components/ManipulationPanel";
 import { initFields } from "./utils";
+import { IPosition } from "./utils/types";
 
-const fields = initFields(35);
+const initialPosition = { x: 17, y: 17 };
+const initialValues = initFields(35, initialPosition);
 
 function App() {
+  const [fields, setFields] = useState(initialValues);
+  const [position, setPosition] = useState<IPosition>();
+  const [tick, setTick] = useState(0);
+  const defaultInterval = 100;
+
+  const timer = useRef<NodeJS.Timeout>();
+
+  const unsubscribe = () => {
+    if (!timer) {
+      return;
+    }
+    clearInterval(timer.current as number | undefined);
+  };
+
+  useEffect(() => {
+    setPosition(initialPosition);
+    timer.current = setInterval(() => {
+      setTick((tick) => tick + 1);
+    }, defaultInterval);
+    return unsubscribe;
+  }, []);
+
+  useEffect(() => {
+    if (!position) {
+      return;
+    }
+    goUp();
+  }, [tick]);
+
+  const goUp = () => {
+    const { x, y } = position!;
+    const nextY = Math.max(y - 1, 0);
+    fields[y][x] = "";
+    fields[nextY][x] = "snake";
+    setPosition({ x, y: nextY });
+    setFields(fields);
+  };
+
   return (
     <div className="App">
       <header className="header">
@@ -19,6 +59,9 @@ function App() {
       <main className="main">
         <Field fields={fields} />
       </main>
+      <div style={{ padding: "16px" }}>
+        <button onClick={goUp}>進む</button>
+      </div>
       <footer className="footer">
         <Button />
         <ManipulationPanel />
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,9 +1,11 @@
-export const initFields = (fieldSize: number) => {
+import { IPosition } from "./types";
+export const initFields = (fieldSize: number, initialPosition: IPosition) => {
   const fields: string[][] = [];
   for (let i = 0; i < fieldSize; i++) {
     const cols = new Array(fieldSize).fill("");
     fields.push(cols);
   }
+  fields[initialPosition.y][initialPosition.x] = "snake";
 
   return fields;
 };
